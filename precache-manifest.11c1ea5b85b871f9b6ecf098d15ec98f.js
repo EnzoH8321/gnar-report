a20a784478371a42d7d38c9f383aa2511905ee24
Enzo Herrera <enzoherrera123@gmail.com>
2020-07-02 15:01:21 -0700

    Updates

---
+++ precache-manifest.11c1ea5b85b871f9b6ecf098d15ec98f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18c98d68a6808c2737d299a0f3f3813",
+    "url": "/gnar-report/index.html"
+  },
+  {
+    "revision": "39f50d35b7ab045bebeb",
+    "url": "/gnar-report/static/css/main.573b35e5.chunk.css"
+  },
+  {
+    "revision": "77acfd8e31d91ee9e4ef",
+    "url": "/gnar-report/static/js/2.e2a3a3fb.chunk.js"
+  },
+  {
+    "revision": "069e703a7ca014ab76714f20e2a39562",
+    "url": "/gnar-report/static/js/2.e2a3a3fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f50d35b7ab045bebeb",
+    "url": "/gnar-report/static/js/main.116944b7.chunk.js"
+  },
+  {
+    "revision": "2f02a107961c87b9ba2e",
+    "url": "/gnar-report/static/js/runtime-main.ea9aeb5c.js"
+  }
+]);
